5755359fb47999a059c89451e39d0b08a30d39cd
Murilo Pantoja Carneiro <158524340+devmirilo@users.noreply.github.com>
2024-06-14 23:12:38 -0300

    commit 1

--- main.c
+++ main.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int contarPalavras(char str[]) {
-    int contador = 0;
-    int i;
 
-    for (i = 0; str[i] != '\0'; i++) {
-        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= '0' && str[i] <= '9')) {
-            if (str[i + 1] == ' ' || str[i + 1] == '\0') {
-                contador++;
-            }
-        }
-    }
-
-    return contador;
+int main(int argc, char *argv[]) {
+	char str[100];
+	int i, cont = 0;	
+	int palavra = 0;
+	
+	puts("Digite uma frase: ");
+	fgets(str, sizeof(str), stdin);
+	
+	str[strcspn(str, "\n")] = '\0';
+	
+	for(i = 0; str[i] != '\0'; i++){
+		if(isspace(str[i])){
+			palavra = 0;
+		}
+		else if(!palavra){
+			palavra = 1;
+			cont++;
+		}
+	}
+	printf("O numero de palavras e de %d", cont);
+	
+	return 0;
 }
-
-int main() {
-    char str[100];
-    printf("Digite um texto: ");
-    gets(str);
-    int palavras = contarPalavras(str);
-    printf("Numero de palavras: %d\n", palavras);
-    return 0;
-}
-
